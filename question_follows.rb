8eb3fa945187d6a9107c0a1afc4fe4c5a4e37fd9
Nishant Racherla <nishant.racherla@gmail.com>
2022-11-02 15:46:07 -0700

    added queries to question_follows

--- question_follows.rb
+++ question_follows.rb
@@ -1,4 +1,5 @@
 require_relative "questions_db.rb"
+require_relative "user.rb"
 
 class QuestionFollows
 
@@ -14,14 +15,36 @@ class QuestionFollows
     QuestionFollows.new(data)
   end
 
+  def self.followers_for_question_id(question_id)
+    data = QuestionsDB.instance.execute(<<-SQL, question_id)
+      SELECT users.id, users.fname, users.lname
+      FROM question_follows
+      JOIN users
+      ON question_follows.user_id = users.id
+      WHERE question_id = ?
+    SQL
+    return nil unless data.length > 0
+    data.map { |datum| User.new(datum) }
+  end
+
+  def self.followed_questions_for_user_id(user_id)
+    data = QuestionsDB.instance.execute(<<-SQL, user_id)
+      SELECT questions.id, questions.title, questions.body, questions.author_user_id
+      FROM question_follows
+      JOIN questions
+      ON question_follows.question_id = questions.id
+      WHERE user_id = ?
+    SQL
+    return nil unless data.length > 0
+    data.map { |datum| User.new(datum) }
+  end
+
   attr_accessor :names
 
   def initialize(data)
-
     @names = data.map do |datum|
         "#{datum["fname"]} #{datum["lname"]}"
     end
-
   end
 
 end
