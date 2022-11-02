cda03cd3a97cf9621cd4731def4a6662b893ce23
Thomas Conger <t.e.conger@gmail.com>
2022-11-02 16:51:32 -0700

    added question_likes::likers_for_question_id

--- question_likes.rb
+++ question_likes.rb
@@ -1,4 +1,5 @@
 require_relative "questions_db.rb"
+require_relative "user.rb"
 
 class QuestionLikes
 
@@ -14,14 +15,23 @@ class QuestionLikes
     QuestionLikes.new(data)
   end
 
+  def self.likers_for_question_id(question_id)
+    data = QuestionsDB.instance.execute(<<-SQL, question_id)
+      SELECT users.id, users.fname, users.lname
+      FROM question_likes
+      JOIN users
+      ON question_likes.participant_user_id = users.id
+      WHERE question_likes.question_id = ?
+    SQL
+    data.length > 0 ? data.map { |datum| User.new(datum) } : nil
+  end
+
   attr_accessor :names
 
   def initialize(data)
-
     @names = data.map do |datum|
         "#{datum["fname"]} #{datum["lname"]}"
     end
-
   end
 
 end
